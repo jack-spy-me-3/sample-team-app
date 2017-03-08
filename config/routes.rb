324e4a883d041592bdd7fc7324d7a9ec464af468
Michael Yatco <mjyatco@gmail.com>
2017-03-07 20:48:27 -0500

    Added tables controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  get "/", to: "pages#index"
+  root to: "tables#index"
+
+  get "/tables" to: "tables#index"
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
